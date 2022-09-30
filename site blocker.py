337e30e6bdb4aa75cfc6c1d8d14e673964e62215
criztyrajuabraham <cristyraju4@gmail.com>
2022-09-30 15:29:31 +0530

    hello

---
+++ site blocker.py
@@ -0,0 +1,47 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('500x300')
+root.wm_resizable(False, False)
+root.title("Website Blocker")
+Label(root, text=' Website Blocker ', font='arial 20 bold').pack()
+host_path = "C:\Windows\System32\drivers\etc\hosts"
+ip_address = '127.0.0.1'
+Label(root, text='Enter Website : ', font='arial 13 bold').place(x=5, y=60)
+Websites = Text(root, font='arial 12 bold', height='2', width='40')
+Websites.place(x=140, y=60)
+
+
+
+
+# def Blocker():
+#     web_lists = Websites.get(1.0, END)
+#     Website = list(web_lists.split(","))
+#     with open(ho_pa , 'r+') as host_file:
+#         file_cont = host_file.read()
+#         for web in Website:
+#             if web in file_cont:
+#                 Label(root, text="already blocked", font='arial 12 bold').place(x=200, y=200)
+#                 pass
+#             else:
+#                 host_file.write(ip_address + " " + web + '\n')
+#                 Label(root, text='Blocked', font="arial 12 bold").place(x=230, y=200)
+
+
+def Blocker():
+    website_lists = Websites.get(1.0,END)
+    Website = list(website_lists.split(","))
+    with open (host_path , 'r+') as host_file:
+        file_content = host_file.read()
+        for website in Website:
+            if website in file_content:
+                Label(root, text = 'Already Blocked' , font = 'arial 12 bold').place(x=200,y=200)
+                pass
+            else:
+                host_file.write(ip_address + " " + website + '\n')
+                Label(root, text = "Blocked", font = 'arial 12 bold').place(x=230,y =200)
+
+block = Button(root, text='block', font="arial 12 bold", pady=5, command=Blocker, width=6, bg='aliceblue',
+               activebackground='sky blue')
+block.place(x=210, y=150)
+root.mainloop()
